69dc0a937b0aee2ac4ebb6a766fc3d407674afe0
Greg Haskins <gregory.haskins@gmail.com>
2019-10-08 21:11:16 -0400

    Organize src into a subfolder
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- src/leiningen/new/re_expo.clj
+++ src/leiningen/new/re_expo.clj
@@ -18,8 +18,8 @@
              ["assets/icon.png" (cp "assets/icon.png")]
              ["assets/shadow-cljs.png" (cp "assets/shadow-cljs.png")]
              ["assets/splash.png" (cp "assets/splash.png")]
-             ["src/{{sanitized}}/app.cljs" (render "app.cljs" data)]
-             ["src/{{sanitized}}/db.cljs" (render "db.cljs" data)]
-             ["src/{{sanitized}}/events.cljs" (render "events.cljs" data)]
-             ["src/{{sanitized}}/subs.cljs" (render "subs.cljs" data)]
-             ["src/reagent/dom.cljs" (render "dom.cljs" data)])))
+             ["src/{{sanitized}}/app.cljs" (render "src/app.cljs" data)]
+             ["src/{{sanitized}}/db.cljs" (render "src/db.cljs" data)]
+             ["src/{{sanitized}}/events.cljs" (render "src/events.cljs" data)]
+             ["src/{{sanitized}}/subs.cljs" (render "src/subs.cljs" data)]
+             ["src/reagent/dom.cljs" (render "src/dom.cljs" data)])))
